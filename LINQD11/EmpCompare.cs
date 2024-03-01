dda4a8b15af2b4518f655aa8a37e23fbc09558c7
Rahma-Mohmed <99695242+rahma-mohmed@users.noreply.github.com>
2024-03-01 13:22:22 +0200

    Update EmpCompare.cs

--- LINQD11/EmpCompare.cs
+++ LINQD11/EmpCompare.cs
@@ -6,25 +6,25 @@ using System.Threading.Tasks;
 
 namespace LINQD11
 {
-    //internal class EmpCompare : IComparer<Employee>
-    //{
-    //    public int Compare(Employee e1 , Employee e2)
-    //    {
-    //        //var e1Year = Convert.ToInt32(e1.EmployeeNo.Split('-')[0]);
-    //        //var e2Year = Convert.ToInt32(e2.EmployeeNo.Split('-')[0]);
+    internal class EmpCompare : IComparer<Employee>
+    {
+       public int Compare(Employee e1 , Employee e2)
+       {
+           var e1Year = Convert.ToInt32(e1.EmployeeNo.Split('-')[0]);
+           var e2Year = Convert.ToInt32(e2.EmployeeNo.Split('-')[0]);
 
-    //        //var e1Seq = Convert.ToInt32(e1.EmployeeNo.Split('-')[2]);
-    //        //var e2Seq = Convert.ToInt32(e2.EmployeeNo.Split('-')[2]);
+           var e1Seq = Convert.ToInt32(e1.EmployeeNo.Split('-')[2]);
+           var e2Seq = Convert.ToInt32(e2.EmployeeNo.Split('-')[2]);
 
 
-    //        //if(e1Year == e2Year)
-    //        //{
-    //        //    return e1Seq.CompareTo(e2Seq);
-    //        //}
-    //        //else
-    //        //{
-    //        //    return e1Year.CompareTo(e2Year);
-    //        //}
-    //    }
-    //}
+           if(e1Year == e2Year)
+           {
+              return e1Seq.CompareTo(e2Seq);
+           }
+           else
+           {
+              return e1Year.CompareTo(e2Year);
+           }
+       }
+    }
 }
